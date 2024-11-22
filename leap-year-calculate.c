4df87198deb6ae519165ad3cad8a3dfd7dd383b0
Emir Kibar <62299912+kibradev@users.noreply.github.com>
2024-11-22 10:05:35 +0300

    Update leap-year-calculate.c

--- leap-year-calculate.c
+++ leap-year-calculate.c
@@ -2,14 +2,21 @@
 
 int main() {
     int yil;
+    char devam;
     
-    printf("Yıl Giriniz: ");
-    scanf("%d", &yil);
-    
-    if ((yil % 4 == 0 && yil % 100 != 0) || yil % 400 == 0)
-        printf("%d Artık Yıldır.\n", yil);
-    else
-        printf("%d Artık Yıl Değildir.\n", yil);
+    do {
+        printf("Yıl Giriniz: ");
+        scanf("%d", &yil);
+        
+        if ((yil % 4 == 0 && yil % 100 != 0) || yil % 400 == 0)
+            printf("%d Artık Yıldır.\n", yil);
+        else
+            printf("%d Artık Yıl Değildir.\n", yil);
+            
+        
+        printf("Devam Etmek İster Misiniz ? (E/H)");
+        scanf(" %c", &devam);
+    } while (devam == 'E' || devam == 'e');
         
     return 0;
 }
