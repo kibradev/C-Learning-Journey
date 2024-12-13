a2267bdfd48894fc389a4bc381176e5f9814918a
Emir Kibar <62299912+kibradev@users.noreply.github.com>
2024-12-13 11:03:23 +0300

    Update grade_average_calculator.c

--- grade_average_calculator.c
+++ grade_average_calculator.c
@@ -22,3 +22,32 @@ int main() {
     
     return 0;
 }
+
+
+// With While Loop // 
+#include <stdio.h>
+
+int main() {
+    int ogrenciNot;
+    int ogrenciSayisi = 10; 
+    int ToplamNot = 0;
+    printf("Kaç Öğrenci Notunu Hesaplamak İstiyorsunuz ? \n");
+    scanf("%d", &ogrenciSayisi);
+    if (ogrenciSayisi > 10){
+        printf("10 Dan fazla öğrenci hesaplanamaz. \n");
+    } else {
+        int i = 1;
+        while(i <= ogrenciSayisi){
+            printf("%d. Öğrenci Notunu Girin", i);
+            scanf("%d", &ogrenciNot);
+            ToplamNot += ogrenciNot;
+            i++;
+        }
+        
+        float Ortalama = ToplamNot / ogrenciSayisi;
+        printf("Sınıfın not ortalaması: %.2f\n", Ortalama);
+    }
+    
+    
+    return 0;
+}
